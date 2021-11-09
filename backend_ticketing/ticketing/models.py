68a17b72023dfcd2354604e8b43e0e31cc54b59c
anderballes98 <ander.ballesteros98@gmail.com>
2021-11-09 13:41:16 -0500

    Se crean modelos para app ticketing

--- backend_ticketing/ticketing/models.py
+++ backend_ticketing/ticketing/models.py
@@ -1,7 +1,8 @@
+import datetime
+
 from django.db import models
 from uuid import uuid4
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class StampModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -10,3 +11,39 @@ class StampModel(models.Model):
 
     class Meta:
         abstract = True
+
+class Company(StampModel):
+    name = models.CharField(unique=True, max_length=50)
+    nit = models.CharField(unique=True, max_length=15)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    email = models.EmailField()
+    description = models.CharField(max_length=150)
+
+class CompanyUser(models.Model):
+    user = models.ForeignKey(User, unique=True, related_name='company_user', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, null=True, related_name='company_user', on_delete=models.SET_NULL)
+    date_join = datetime.datetime.now()
+
+class UserStory(StampModel):
+    name = models.CharField(max_length=70)
+    description = models.TextField(null=True)
+    company = models.ForeignKey(Company, null=True, related_name='user_stories', on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User, null=True, related_name='user_stories', on_delete=models.SET_NULL)
+
+class TicketState(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    description = models.TextField(null=True)
+
+class Ticket(StampModel):
+    name = models.CharField(max_length=70)
+    description = models.TextField(null=True)
+    user_story = models.ForeignKey(UserStory, null=True, related_name='tickets', on_delete=models.SET_NULL)
+    state = models.ForeignKey(TicketState, null=True, default=1, related_name='tickets', on_delete=models.SET_NULL)
+    crated_by = models.ForeignKey(User, null=True, related_name='tickets', on_delete=models.SET_NULL)
+
+class Comment(StampModel):
+    text = models.TextField()
+    ticket = models.ForeignKey(Ticket, null=True, related_name='comments', on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User, null=True, related_name='comments', on_delete=models.SET_NULL)
--- backend_ticketing/users/views/users.py
+++ backend_ticketing/users/views/users.py
@@ -1,10 +1,5 @@
-from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 from users.serializers import UserSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from users.decorators import CheckGroup
-from django.contrib.auth.models import User, Group
 
 
 class UserView(CreateAPIView):

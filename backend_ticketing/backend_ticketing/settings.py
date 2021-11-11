60853142815aa2a4e86c9f33a2ae17773b6e92e7
anderballes98 <ander.ballesteros98@gmail.com>
2021-11-11 17:35:14 -0500

    Se añade cors

--- backend_ticketing/backend_ticketing/settings.py
+++ backend_ticketing/backend_ticketing/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import datetime
 from os.path import expanduser
+
 home = expanduser("~")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = "{home}/databases".format(home=home)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -30,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'clear_cache',
     'ticketing',
     'users',
@@ -54,7 +54,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:4200',
+)
 
 ROOT_URLCONF = 'backend_ticketing.urls'
 
@@ -76,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend_ticketing.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -86,7 +92,6 @@ DATABASES = {
         'NAME': "{base_dir}/{db_name}".format(base_dir=BASE_DIR, db_name='ticketing.db'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -106,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -119,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/

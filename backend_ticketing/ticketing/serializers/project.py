a7e9decfadffb36da95c38c71fcf0c60ebea188c
anderballes98 <ander.ballesteros98@gmail.com>
2021-11-12 03:23:12 -0500

    Se añade la data del usuario al las respuetas de project

--- backend_ticketing/ticketing/serializers/project.py
+++ backend_ticketing/ticketing/serializers/project.py
@@ -1,11 +1,13 @@
 from ticketing.models import Project, Company
 from rest_framework import serializers
 from .company import CompanySerializer
+from users.serializers import UserSerializer
 from django.core.exceptions import ValidationError as ModelValidationError, ObjectDoesNotExist
 
 class ProjectSerializer(serializers.ModelSerializer):
 
     company = CompanySerializer(read_only=True)
+    created_by = UserSerializer(read_only=True)
 
     class Meta:
         model = Project
@@ -14,12 +16,14 @@ class ProjectSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         project = Project(**validated_data)
         context_data = self.context['request'].data
+        user = self.context['request'].user
         if not 'company' in context_data.keys():
             raise serializers.ValidationError({'company': ['This value is required']})
         company_id = context_data['company']
         try:
             company = Company.objects.get(pk=company_id)
             project.company = company
+            project.created_by = user
         except ObjectDoesNotExist:
             raise serializers.ValidationError({'company': ['company doesn\'t exist']})
         except ModelValidationError:
